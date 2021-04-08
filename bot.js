f9f0233a6278de2cfc6a0c6216be55085586f0f1
Derrick K <60911261+Dpk417182@users.noreply.github.com>
2021-04-08 16:47:31 -0400

    Add files via upload

--- bot.js
+++ bot.js
@@ -13,10 +13,13 @@ function readyDiscord() {
 }
 
 const replies = [
-    'I 👀 you!',
-    'Will you wear wigs?',
-    'When will you wear wigs?',
-    'Number 1 Victory Royale, yeah thats right we bout to get down (get down)'
+    'I am... ungraceful. - Gamaken',
+    'The True Measure Of A Shinobi Is Not How He Lives, But How He Dies. - Jiraya Sensai',
+    'Those Who Break The Rules Are Scum... But, Those Who Abandon Their Friends Are Worse Than Scum. - Kakashi Hatake',
+    'Power is not will, it is the phenomenon of physically making things happen. - Madara Uchiha',
+    'Never give up without even trying. Do what you can, no matter how small the effect it may have! - Onoki',
+    'It is only through the eyes of others that our lives have any meaning. – Haku',
+    'Hard work is worthless for those that don\'t believe in themselves. - Naruto Uzumaki'
 ]
 
 client.on('message', gotMessage);
@@ -24,13 +27,13 @@ client.on('message', gotMessage);
 function gotMessage(msg) {
     console.log(msg.content);
     if (msg.channel.id == process.env.CHANNELID) {
-        if (msg.content === '!test') {
+        if (msg.content === '!quote') {
             // msg.reply('I 👀 you!');  // replies to user who initiated the command
             const index = Math.floor(Math.random() * replies.length);   // get a random number between 0 and the replies array's length
             msg.channel.send(replies[index]);   // use that number to send a random message back
         }
-        else if (msg.content === '!upcoming') {
-            msg.channel.send('upcoming command entered!');
+        else if (msg.content === '!schedule') {     // TODO
+            msg.channel.send('The schedule feature will be coming in the future!');
         }
     }
 }
